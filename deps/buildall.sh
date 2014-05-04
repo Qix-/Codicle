7763a5a9850da24933cb567276c7b47efa9b2818
JD Ballard <jd@pixelandline.com>
2014-05-04 03:05:16 -0600

    depend: added --force to build script

--- deps/buildall.sh
+++ deps/buildall.sh
@@ -1,54 +1,64 @@
 #!/bin/bash
 
+# Force?
+[[ "$1" == "--force" ]] || echo "Will fail on error..."
+function checkExit() {
+	if [[ "$1" == "--force" ]]; then
+		echo -e "\x1B[31;1mERROR; --force specified: continuing...\x1B[0m"
+	else
+		exit 1
+	fi
+}
+
 # Ignore CR's in scripts
 #	and ignore DOS style paths
-set -o igncr || exit 1
+set -o igncr || checkExit
 export CYGWIN="${CYGWIN} nodosfilewarning"
 
 # Dependencies check
 if [[ `which node 2>/dev/null | wc -l` -eq 0 ]]; then
 	echo -e "\x1b[31;1mNODE.JS NOT FOUND. CANNOT CONTINUE.\x1b[0m"
-	exit 1
+	checkExit
 fi
 
 if [[ `which npm 2>/dev/null | wc -l` -eq 0 ]]; then
 	echo -e "\x1b[31;1mNPM NOT FOUND. CANNOT CONTINUE.\x1b[0m"
-	exit 1
+	checkExit
 fi
 
 if [[ `which python | wc -l` -eq 0 ]]; then
 	echo -e "\x1b[31;1mPYTHON NOT FOUND. CANNOT CONTINUE.\x1b[0m"
-	exit 1
+	checkExit
 fi
 
 if [[ `python --version 2>&1` != "Python 2.7"* ]]; then
 	echo -e "\x1b[31;1mPYTHON 2.7.x NOT FOUND. CANNOT CONTINUE.\x1b[0m"
-	exit 1
+	checkExit
 fi
 
 if [[ `which clang | wc -l` -eq 0 ]]; then
 	echo -e "\x1b[31;1mCLANG NOT FOUND. CANNOT CONTINUE.\x1b[0m"
-	exit 1
+	checkExit
 fi
 
 if [[ `which grunt 2>/dev/null | wc -l` -eq 0 ]]; then
-	npm install -g grunt-cli || exit 1
+	npm install -g grunt-cli || checkExit
 fi
 
 if [[ `which bower 2>/dev/null | wc -l` -eq 0 ]]; then
-	npm install -g bower || exit 1
+	npm install -g bower || checkExit
 fi
 
 # ACE
 cd ./ace
-npm install || exit 1
-node ./Makefile.dryice.js || exit 1
+npm install || checkExit
+node ./Makefile.dryice.js || checkExit
 cd ..
 
 # Buzz
 cd ./buzz
-npm install || exit 1
-grunt || exit 1
+npm install || checkExit
+grunt || checkExit
 cd ..
 
 # ez_setup
@@ -58,7 +68,7 @@ cd ..
 
 # GitPython
 cd ./GitPython
-python ./setup.py install || exit 1
+python ./setup.py install || checkExit
 cd ..
 
 # Skulpt
@@ -74,25 +84,25 @@ cd ..
 #	I'm doing this in here because forking the repo
 #	for something small like this isn't worth it.
 cd ./skulpt
-cat skulpt.py | sed -e "s/\#jsengine/jsengine/" | sed -e "s/rhino/node/g" | python - dist -v || exit 1
+cat skulpt.py | sed -e "s/\#jsengine/jsengine/" | sed -e "s/rhino/node/g" | python - dist -v || checkExit
 cd ..
 
 # PHP.js
 cd ./phpjs
-npm install || exit 1
-grunt --force || exit 1
+npm install || checkExit
+grunt --force || checkExit
 cd ..
 
 # Emscripten
 cd ./emscripten
-npm install || exit 1
+npm install || checkExit
 emmake
 export PATH=$PATH:`pwd`
 cd ..
 
 # asm.js
 cd ./asmjs
-npm install || exit 1
+npm install || checkExit
 cd ..
 
 # Log
